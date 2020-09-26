d1ee80696765245e99ee660680341c4073a46c22
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 15:19:22 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:19:22 up  1:30,  1 user,  load average: 0.21, 0.29, 0.41 720d649e9f6c48009df8fb14923ff4fb2ff4a443

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,7 +37,7 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_si(char *args) {
-	char* arg = strtok(NULL,  "  ");
+	char* arg = strtok(args,  " ");
 	Log("%zu", strlen(arg));
 	if(strlen(arg) > 1) {
 		Log("Wrong params!");
