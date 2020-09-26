176457bb39b5dbbb57e2d48ead49fec331df85a0
3019244253-Guo Yupeng <1368179767@qq.com>
2020-09-26 14:39:02 +0800

    first step

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -21,7 +21,6 @@ typedef struct {
 			uint16_t _16;
 			uint8_t _8[2];
 		} gpr[8];
-
 		/* Do NOT change the order of the GPRs' definitions. */
 		struct {
 		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
