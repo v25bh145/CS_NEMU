b9e8e2e85c1de018a09418026a8ad0f71f40737c
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 09:42:23 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:42:23 up  1:34,  1 user,  load average: 0.74, 0.54, 0.47 3801ff45f21dd1412fc2df88d8f6614e286ed578

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -164,7 +164,7 @@ static int cmd_w (char* args) {
 	if(arg[0] == '*') {
 		char* substr = (char *)malloc(5 * sizeof(char));
 		int i;
-		for(i = 1; i < strlen(arg); i++) {
+		for(i = 1; i <= strlen(arg); i++) {
 			substr[i - 1] = arg[i];
 		}
 		Log("%d", (int)strlen(arg));
