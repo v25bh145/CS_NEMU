b32c84cd48fdc66f47a00f65a00d0ed83f4e1ce7
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 09:33:59 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:33:59 up 16 min,  1 user,  load average: 1.37, 1.47, 1.19 7d6a87da312a12325d308a643f27f94230ad44ff

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -182,7 +182,7 @@ static int cmd_w (char* args) {
 			Log("2");
 		}
 		if(ptr != NULL) {
-			Log("help: w addr %I32u", addr);
+			Log("help: w addr %I64u\n", addr);
 			// Log("Wrong params!");
 			// Log("help: w addr");
 			return 1;
