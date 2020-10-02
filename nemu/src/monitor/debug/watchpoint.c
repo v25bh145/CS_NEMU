5229de72704e93eff62724f9897d5ab6a9ac71a9
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 09:45:56 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:45:56 up 28 min,  1 user,  load average: 1.36, 1.95, 1.69 530382e156f158d0dc37cddb8af1111773912456

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -23,31 +23,39 @@ void init_wp_pool() {
 WP* get_wp_head () {
 	return head;
 }
+void printAllPool () {
+	WP* h;
+	for(h = head; h != NULL; h = h->next) {
+		Log("%d ", h->addr);
+	}
+	Log("over");
+}
 int setBreakpoint(uint32_t step) {
-	Log("%d", step);
+	// Log("%d", step);
 	current++;
-	// wp_pool[current - 1].addr = step;
-	// if(head == NULL) {
-	// 	head = &wp_pool[current - 1];
-	// } else {
-	// 	//sort
-	// 	WP* h;
-	// 	for(h = head; h != NULL; h = h->next) {
-	// 		WP* nex = h->next;
-	// 		if(nex == NULL) {
-	// 			//insert into the last one
-	// 			h->next = &wp_pool[current - 1]; 
-	// 		} else {
-	// 				if(nex->addr > wp_pool[current - 1].addr) {
-	// 					//insert into the pre of h2
-	// 					h->next = &wp_pool[current - 1];
-	// 					wp_pool[current - 1].next = nex;
-	// 					break;
-	// 				} else {
-	// 					continue;
-	// 				}
-	// 		}
-	// 	}
-	// }
+	wp_pool[current - 1].addr = step;
+	if(head == NULL) {
+		head = &wp_pool[current - 1];
+	} else {
+		//sort
+		WP* h;
+		for(h = head; h != NULL; h = h->next) {
+			WP* nex = h->next;
+			if(nex == NULL) {
+				//insert into the last one
+				h->next = &wp_pool[current - 1]; 
+			} else {
+					if(nex->addr > wp_pool[current - 1].addr) {
+						//insert into the pre of h2
+						h->next = &wp_pool[current - 1];
+						wp_pool[current - 1].next = nex;
+						break;
+					} else {
+						continue;
+					}
+			}
+		}
+	}
+
 	return 0;
 }
