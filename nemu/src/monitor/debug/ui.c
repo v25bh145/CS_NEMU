825a5fcc4d65b283a5bfccee767d196c943afee3
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 09:35:04 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:35:04 up 17 min,  1 user,  load average: 1.56, 1.50, 1.21 72da534cf1f58aca9c4901bb3d1d3b21381c6520

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -187,7 +187,7 @@ static int cmd_w (char* args) {
 			// Log("help: w addr");
 			// return 1;
 		// }
-		Log("help: w addr %I64u\n", addr);
+		Log("help: w addr %8x\n", addr);
 		Log("%s", substr);
 		setBreakpoint(addr);
 	} else {
