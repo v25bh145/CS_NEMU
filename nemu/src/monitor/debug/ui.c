e34188c8e404c0ae0c40a48dc03e2ff53b2f9d72
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 17:13:28 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:13:28 up  8:26,  1 user,  load average: 0.56, 0.59, 0.52 aebf4dc0a07395b8a9ff3bb3bc72ec07fce38ce

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -12,48 +12,53 @@
 void cpu_exec(uint32_t);
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
-char* rl_gets() {
+char *rl_gets()
+{
 	static char *line_read = NULL;
 
-	if (line_read) {
+	if (line_read)
+	{
 		free(line_read);
 		line_read = NULL;
 	}
 
 	line_read = readline("(nemu) ");
 
-	if (line_read && *line_read) {
+	if (line_read && *line_read)
+	{
 		add_history(line_read);
 	}
 
 	return line_read;
 }
 
-static int cmd_c(char *args) {
+static int cmd_c(char *args)
+{
 	cpu_exec(-1);
 	return 0;
 }
 
-static int cmd_q(char *args) {
+static int cmd_q(char *args)
+{
 	return -1;
 }
 
-static int cmd_si(char *args) ;
+static int cmd_si(char *args);
 
-static int cmd_info(char* args);
+static int cmd_info(char *args);
 
-static int cmd_p(char *args) ;
+static int cmd_p(char *args);
 
-static int cmd_x(char* args);
+static int cmd_x(char *args);
 
-static int cmd_w(char* args);
+static int cmd_w(char *args);
 
-static int cmd_d(char* args);
+static int cmd_d(char *args);
 
 // static int cmd_bt(char* args);
 
 //external
-WP* get_wp_head();
+WP *get_wp_head();
 int setWatchpoint(swaddr_t step);
 void printAllPool();
 int delWatchpoint(int count);
@@ -61,17 +66,18 @@ long long int expr_cmd(char *e, bool *success);
 
 static int cmd_help(char *args);
 
-static struct {
+static struct
+{
 	char *name;
 	char *description;
-	int (*handler) (char *);
-} cmd_table [] = {
-	{ "help", "Display informations about all supported commands", cmd_help },
-	{ "c", "Continue the execution of the program", cmd_c },
-	{ "q", "Exit NEMU", cmd_q },
+	int (*handler)(char *);
+} cmd_table[] = {
+	{"help", "Display informations about all supported commands", cmd_help},
+	{"c", "Continue the execution of the program", cmd_c},
+	{"q", "Exit NEMU", cmd_q},
 
 	/* TODO: Add more commands */
-	{ "si", "Continue the execution for one step of the program", cmd_si},
+	{"si", "Continue the execution for one step of the program", cmd_si},
 	{"info", "Print info from regs and wds", cmd_info},
 	{"w", "Set a watchpoint", cmd_w},
 	{"d", "Del a watchpoint", cmd_d},
@@ -82,20 +88,26 @@ static struct {
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
-static int cmd_help(char *args) {
+static int cmd_help(char *args)
+{
 	/* extract the first argument */
 	char *arg = strtok(NULL, " ");
 	int i;
 
-	if(arg == NULL) {
+	if (arg == NULL)
+	{
 		/* no argument given */
-		for(i = 0; i < NR_CMD; i ++) {
+		for (i = 0; i < NR_CMD; i++)
+		{
 			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
 		}
 	}
-	else {
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(arg, cmd_table[i].name) == 0) {
+	else
+	{
+		for (i = 0; i < NR_CMD; i++)
+		{
+			if (strcmp(arg, cmd_table[i].name) == 0)
+			{
 				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
 				return 0;
 			}
@@ -105,17 +117,20 @@ static int cmd_help(char *args) {
 	return 0;
 }
 
-static int cmd_si(char *args) {
-	char* arg = strtok(args,  " ");
-	char* argOverflow = strtok(NULL, " ");
-	if(argOverflow != NULL) {
+static int cmd_si(char *args)
+{
+	char *arg = strtok(args, " ");
+	char *argOverflow = strtok(NULL, " ");
+	if (argOverflow != NULL)
+	{
 		Log("Wrong params!");
 		Log("help: si [N>0]");
 		return 1;
 	}
 
 	int step = atoi(arg);
-	if(step == 0 ){
+	if (step == 0)
+	{
 		Log("Wrong params!");
 		Log("help: si [N>0]");
 		return 1;
@@ -124,15 +139,18 @@ static int cmd_si(char *args) {
 	return 0;
 }
 
-static int cmd_info(char* args) {
-	char* arg = strtok(args,  " ");
-	char* argOverflow = strtok(NULL, " ");
-	if(argOverflow != NULL) {
+static int cmd_info(char *args)
+{
+	char *arg = strtok(args, " ");
+	char *argOverflow = strtok(NULL, " ");
+	if (argOverflow != NULL)
+	{
 		Log("Wrong params!");
 		Log("help: info [SUBCMD {r, w}]");
 		return 1;
 	}
-	if(!strcmp(arg, "r")) {
+	if (!strcmp(arg, "r"))
+	{
 		printf("Now print the regs info: \n");
 		printf("eax: %ld\n", (long int)cpu.eax);
 		printf("ecx: %ld\n", (long int)cpu.ecx);
@@ -144,72 +162,94 @@ static int cmd_info(char* args) {
 		printf("edi: %ld\n", (long int)cpu.edi);
 		printf("eip: %8x\n", cpu.eip);
 		return 0;
-	} else if(!strcmp(arg, "w")) {
+	}
+	else if (!strcmp(arg, "w"))
+	{
 		printf("Now print the watchpoint info: \n");
 		printAllPool();
 		return 0;
-	} else {
+	}
+	else
+	{
 		Log("Wrong params!");
 		Log("help: info [SUBCMD {r, w}]");
 		return 0;
 	}
 }
 
-static int cmd_w (char* args) {
+static int cmd_w(char *args)
+{
 	bool flag = false;
-	char* arg = strtok(args,  " ");
-	char* argOverflow = strtok(NULL, " ");
-	if(argOverflow != NULL || arg == NULL) {
+	char *arg = strtok(args, " ");
+	char *argOverflow = strtok(NULL, " ");
+	if (argOverflow != NULL || arg == NULL)
+	{
 		Log("Wrong params!");
 		Log("help: w addr");
 		return 1;
 	}
-	if(arg[0] == '*') {
-		char* substr = (char *)malloc(strlen(arg) * sizeof(char));
-		char* substrFor0x = (char *)malloc(strlen(arg) * sizeof(char));
+	if (arg[0] == '*')
+	{
+		char *substr = (char *)malloc(strlen(arg) * sizeof(char));
+		char *substrFor0x = (char *)malloc(strlen(arg) * sizeof(char));
 		int i;
-		for(i = 1; i <= strlen(arg); i++) {
+		for (i = 1; i <= strlen(arg); i++)
+		{
 			substr[i - 1] = arg[i];
-			if(i > 2) substrFor0x[i - 3] = arg[i];
+			if (i > 2)
+				substrFor0x[i - 3] = arg[i];
 		}
 		uint32_t addr;
-		if((substr[1] == 'x' && substr[0] == '0' )|| (substr[1] == 'X' && substr[0] == '0')) {
+		if ((substr[1] == 'x' && substr[0] == '0') || (substr[1] == 'X' && substr[0] == '0'))
+		{
 			sscanf(substrFor0x, "%x", &addr);
-		} else {
+		}
+		else
+		{
 			sscanf(substr, "%d", &addr);
 		}
-		if(addr != 0) {
+		if (addr != 0)
+		{
 			flag = true;
 			setWatchpoint(addr);
 		}
-	} else {
-		char* regStr[] = {"eax", "ebx", "ecx", "edx", "esp", "ebp", "esi", "edi", "eip"};
+	}
+	else
+	{
+		char *regStr[] = {"eax", "ebx", "ecx", "edx", "esp", "ebp", "esi", "edi", "eip"};
 		uint32_t regs[] = {cpu.eax, cpu.ebx, cpu.ecx, cpu.edx, cpu.esp, cpu.ebp, cpu.esi, cpu.edi, cpu.eip};
 		int i;
-		for(i = 0; i < strlen(arg); i++) 
+		for (i = 0; i < strlen(arg); i++)
 			arg[i] = tolower(arg[i]);
-		for(i = 0; i < 9; i++) {
-			if(!strcmp(arg, regStr[i])) {
+		for (i = 0; i < 9; i++)
+		{
+			if (!strcmp(arg, regStr[i]))
+			{
 				setWatchpoint(regs[i]);
 				flag = true;
 				break;
 			}
 		}
 	}
-	if(flag == false) {
+	if (flag == false)
+	{
 		Log("Wrong params!");
 		Log("help: w addr");
 		return 1;
-	}else {
+	}
+	else
+	{
 		return 0;
 	}
-} 
+}
 
-static int cmd_d(char* args) {
-	char* arg = strtok(args,  " ");
+static int cmd_d(char *args)
+{
+	char *arg = strtok(args, " ");
 	int count;
-	char* argOverflow = strtok(NULL, " ");
-	if(argOverflow != NULL || arg == NULL) {
+	char *argOverflow = strtok(NULL, " ");
+	if (argOverflow != NULL || arg == NULL)
+	{
 		Log("Wrong params!");
 		Log("help: w addr");
 		return 1;
@@ -219,20 +259,26 @@ static int cmd_d(char* args) {
 	return 0;
 }
 
-void ui_mainloop() {
-	while(1) {
+void ui_mainloop()
+{
+	while (1)
+	{
 		char *str = rl_gets();
 		char *str_end = str + strlen(str);
 
 		/* extract the first token as the command */
 		char *cmd = strtok(str, " ");
-		if(cmd == NULL) { continue; }
+		if (cmd == NULL)
+		{
+			continue;
+		}
 
 		/* treat the remaining string as the arguments,
 		 * which may need further parsing
 		 */
 		char *args = cmd + strlen(cmd) + 1;
-		if(args >= str_end) {
+		if (args >= str_end)
+		{
 			args = NULL;
 		}
 
@@ -241,44 +287,66 @@ void ui_mainloop() {
 		sdl_clear_event_queue();
 #endif
 		int i;
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(cmd, cmd_table[i].name) == 0) {
-				if(cmd_table[i].handler(args) < 0) { return; }
+		for (i = 0; i < NR_CMD; i++)
+		{
+			if (strcmp(cmd, cmd_table[i].name) == 0)
+			{
+				if (cmd_table[i].handler(args) < 0)
+				{
+					return;
+				}
 				break;
 			}
 		}
 
-		if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+		if (i == NR_CMD)
+		{
+			printf("Unknown command '%s'\n", cmd);
+		}
 	}
 }
 
-static int cmd_p(char *args) {
+static int cmd_p(char *args)
+{
 	bool status;
 	long long int res;
 	res = expr_cmd(args, &status);
-	if(!status) {
+	if (!status)
+	{
 		Log("error: please input right expr!");
 		return 2;
-	} else {
+	}
+	else
+	{
 		Log("%lld", res);
 		return 0;
 	}
 }
 
-static int cmd_x(char *args) {
-	if(strlen(args) < 3) {
+static int cmd_x(char *args)
+{
+	if (strlen(args) < 3)
+	{
 		Log("Wrong params!");
 		Log("help: x N expr");
 		return 1;
-	} 
+	}
 	int i;
-	char* strN = (char *)malloc(strlen(args + 1) * sizeof(char));
-	char* expr = (char *)malloc(strlen(args + 1) * sizeof(char));
+	char *strN = (char *)malloc(strlen(args + 1) * sizeof(char));
+	char *expr = (char *)malloc(strlen(args + 1) * sizeof(char));
 	uint32_t n;
-	for(i = 0; args[i] != ' '; i++) strN[i] = args[i];
+	bool status;
+	for (i = 0; args[i] != ' '; i++)
+		strN[i] = args[i];
 	strN[i++] = '\0';
-	for(; i <= strlen(args); i++){ expr[i - strlen(strN) - 1] = args[i];Log("%d %c", i, args[i]);}
+	for (; i <= strlen(args); i++)
+		expr[i - strlen(strN) - 1] = args[i];
 	n = atoi(strN);
-	Log("%d  ||| %s", n, expr);
+	long long int val = expr_cmd(expr, &status);
+	if(!status) {
+		Log("error: please input right expr!");
+		return 2;
+	}
+	Log("%lld", val);
 	return 0;
 }
