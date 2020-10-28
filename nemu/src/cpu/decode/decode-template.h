e399e042c9f33414e12b9dcaadeb7705a723f464
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 16:18:17 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:18:17 up  1:01,  1 user,  load average: 1.19, 0.82, 0.80 f301962097529268f01b1ec552d991f6ed7688f9

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -194,7 +194,7 @@ make_helper(concat(decode_modrm_i_, SUFFIX)) {
 	// Log("%x", instr_fetch(eip, 1));
 	// ModR_M modrm;
 	read_ModR_M(eip, op_dest, op_src2);
-	// eip++;
+	eip++;
 	int len = concat(decode_i_, SUFFIX)(eip);
 	return len;
 }
