5ce2eb40f2c07051bd46a910974a20acdca7f006
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 16:13:07 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:13:07 up 55 min,  1 user,  load average: 1.54, 0.91, 0.83 8ec0921e3baffdacc2e7fc49b70e5c6ec495207

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -88,7 +88,7 @@ int read_ModR_M(swaddr_t eip, Operand *rm, Operand *reg) {
 	m.val = instr_fetch(eip, 1);
 	reg->type = OP_TYPE_REG;
 	reg->reg = m.reg;
-	panic("DEBUG: mod %x R_M %x reg %x val %x", m.mod, m.R_M, m.reg, m.val);
+	Log("DEBUG: mod %x R_M %x reg %x val %x", m.mod, m.R_M, m.reg, m.val);
 	if(m.mod == 3) {
 		rm->type = OP_TYPE_REG;
 		rm->reg = m.R_M;
