e4bf8f0c5ddf64bad097836a6862458aaecc826c
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-31 19:55:14 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:55:14 up 32 min,  1 user,  load average: 0.77, 1.13, 1.01 ca321d53efc3fc281e189d18216aae849b30214

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -219,16 +219,16 @@ make_helper(concat(decode_addr2r_, SUFFIX)) {
 	return instr_len;
 }
 make_helper(concat(decode_rmb2r_,SUFFIX)) {
-	panic("len: %x", eip);
-	int len_addr = read_ModR_M(eip, op_src2, op_dest);
+	// panic("len: %x", eip);
+	int len_addr = read_ModR_M(eip - 1, op_src2, op_dest);
 	panic("len: %d", len_addr);
 	//op_src
 	len_addr += concat(decode_r_, SUFFIX) (eip + len_addr);
 	return len_addr;
 }
 make_helper(concat(decode_rmw2r_,SUFFIX)) {
-	panic("len: %x", eip);
-	int len_addr = read_ModR_M(eip, op_src2, op_dest);
+	// panic("len: %x", eip);
+	int len_addr = read_ModR_M(eip - 1, op_src2, op_dest);
 	panic("len: %d", len_addr);
 	//op_src
 	len_addr += concat(decode_r_, SUFFIX) (eip + len_addr);
