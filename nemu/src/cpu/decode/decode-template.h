8bfc109e8663ece4fdde0ce604b48892a10f57d0
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-31 19:55:54 +0800

    >  run 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:55:54 up 33 min,  1 user,  load average: 0.84, 1.12, 1.01 8c04d68fc945efc7bc1b09d6d48fa087117c6f6

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -219,7 +219,7 @@ make_helper(concat(decode_addr2r_, SUFFIX)) {
 	return instr_len;
 }
 make_helper(concat(decode_rmb2r_,SUFFIX)) {
-	// panic("len: %x", eip);
+	panic("len: %x", eip - 1);
 	int len_addr = read_ModR_M(eip - 1, op_src2, op_dest);
 	panic("len: %d", len_addr);
 	//op_src
@@ -227,7 +227,7 @@ make_helper(concat(decode_rmb2r_,SUFFIX)) {
 	return len_addr;
 }
 make_helper(concat(decode_rmw2r_,SUFFIX)) {
-	// panic("len: %x", eip);
+	// panic("len: %x", eip - 1);
 	int len_addr = read_ModR_M(eip - 1, op_src2, op_dest);
 	panic("len: %d", len_addr);
 	//op_src
