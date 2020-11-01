0b39c5c09cca42d576a763eaadbad94238c1759a
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-02 00:36:29 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:36:29 up  3:03,  1 user,  load average: 1.06, 0.56, 0.64 3d7f38552e7a0e8879875f09d7eee460a01380ca

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -32,7 +32,7 @@ make_helper(concat(decode_si_, SUFFIX)) {
 	 *
 	op_src->simm = ???
 	 */
-	// panic("please impleme me %d", instr_fetch(eip, DATA_BYTE));
+	panic("please impleme me %d", (DATA_TYPE_S)instr_fetch(eip, DATA_BYTE));
 	op_src->simm = instr_fetch(eip, DATA_BYTE);
 	op_src->val = op_src->simm;
 
