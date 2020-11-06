1faabfc74d129c9f46a7faac3c72fcf5abc6167f
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-06 17:18:32 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:18:32 up  1:32,  1 user,  load average: 0.67, 0.79, 0.87 f69f00536a5b3c5f1f31a0d8c20d2ce054b18f9e

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -100,7 +100,7 @@ char* get_function_by_addr(uint32_t ebp, bool* success) {
 	for(i = 0; i < nr_symtab_entry; i++) {
 		if(((int)symtab[i].st_info & 0xf) == STT_FUNC) {
 			Log("get function size %x value %x", symtab[i].st_size, symtab[i].st_value);
-			return "qwq";
+			// return "qwq";
 		}
 	}
 	success = false;
