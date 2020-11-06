5ea43644661afeb368f27098d1f9eaf3fa9fc2a7
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-06 17:32:13 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:32:12 up  1:46,  1 user,  load average: 1.21, 1.16, 1.00 e34e0a897dcc233269d070d47b489d8de78078d

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -100,6 +100,7 @@ long long get_var_by_name(char* var_name, bool*success) {
 char* get_function_by_addr(uint32_t ebp, bool* success) {
 	int i = 0;
 	uint32_t instr_addr = swaddr_read(ebp + 4, 4);
+	Log("instr %x", instr_addr);
 	for(i = 0; i < nr_symtab_entry; i++) {
 		if(((int)symtab[i].st_info & 0xf) == STT_FUNC && symtab[i].st_value <= instr_addr && instr_addr <= symtab[i].st_value + symtab[i].st_size) {
 			Log("get function size %x value %x name %s", symtab[i].st_size, symtab[i].st_value, strtab + symtab[i].st_name);
