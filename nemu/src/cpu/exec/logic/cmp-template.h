551b3e821c031b49ff55da5430a89cccc05d48be
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-08 20:50:46 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:50:46 up 32 min,  1 user,  load average: 0.27, 0.81, 0.87 8646d4afc9a703ce60951c8d6f9543ac99a6276c

--- nemu/src/cpu/exec/logic/cmp-template.h
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -6,8 +6,8 @@ static void do_execute() {
     //op_dest val has been swap to val...
     //val2: op_src->val val1: op_dest->val
     DATA_TYPE result = op_dest->val - op_src->val;
-    Log("cmp res %d dest %d pos %d src %d pos %d with eip: %x", result, op_dest->val, op_dest->reg, op_src->val, op_src->reg, cpu.eip);
-    Log("spj: %d", MEM_R(cpu.ebp - 0x1008));
+    // Log("cmp res %d dest %d pos %d src %d pos %d with eip: %x", result, op_dest->val, op_dest->reg, op_src->val, op_src->reg, cpu.eip);
+    // Log("spj: %d", MEM_R(cpu.ebp - 0x1008));
     cpu.psw->ZF = !result;
     cpu.psw->CF = op_dest -> val < op_src -> val;
     cpu.psw->SF = result >> ((DATA_BYTE << 3) - 1);
