68e548998dc1c68419affbbe2859c4a0abc13d26
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-08 20:43:54 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:43:54 up 25 min,  1 user,  load average: 1.07, 1.29, 0.96 89bbf8bd910b1966ed47b47380c3197d7d577791

--- nemu/src/cpu/exec/instr-jmp/jcc-template.h
+++ nemu/src/cpu/exec/instr-jmp/jcc-template.h
@@ -94,6 +94,7 @@ static void do_execute() {
                 break;
                 // panic("JMP");
             case -0x70:
+                cpu.eip = rel;
                 panic("JMP 0xFF");
             default:
                 panic("UNKNOWN ERROR %d", op_src->abstract_instr_enum);
@@ -185,7 +186,8 @@ static void do_execute() {
                 cpu.eip += rel;
                 break;
             case -0x80:
-                panic("JMP 0xFF %x", op_src->val);
+                cpu.eip = rel;
+                // panic("JMP 0xFF %x", op_src->val);
             default:
                 panic("UNKNOWN ERROR %d", op_src->abstract_instr_enum);
         }   
