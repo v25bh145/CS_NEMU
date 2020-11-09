13fe3399038cd82ad01ab077ecdd2c21a4ad5546
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-09 21:03:26 +0800

    >  compile NEMU 3019244253 v25bh145 Linux v25bh145-VirtualBox 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:03:26 up 43 min,  1 user,  load average: 1.82, 1.11, 0.92 d0c7f4b09509ab06e80e763766412dd9d16b56d6

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -24,7 +24,7 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
 	// if(cpu.eip == 1050686 && len == 0)
-		Log("awa len: %d %x", (unsigned int)addr, cpu.eip);
+		// Log("awa len: %d %x", (unsigned int)addr, cpu.eip);
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
